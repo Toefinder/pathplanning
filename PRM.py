df8bf84975742c18fdbcd07682e6f77fdf2f3f3b
Toefinder <lequanganhok@gmail.com>
2020-10-13 19:29:11 +0800

    Added Dijkstra search

--- PRM.py
+++ PRM.py
@@ -138,6 +138,89 @@ def generate_road_map(sample_x, sample_y, rr):
 
     return road_map
 
+def dijkstra_planning(sx, sy, gx, gy, full_road_map, full_sample_x, full_sample_y):
+    """
+    :param sx: starting x coordinate
+    :param sy: starting y coordinate
+    :param gx: goal x coordinate
+    :param gy: goal y coordinate
+    :type sx, sy, gy, gy: float
+    :param full_road_map: road_map after adding information from query
+    :type full_road_map: list of lists
+    :param full_sample_x: sample_x after adding information from query
+    :param full_sample_y: sample_y after adding information from query
+    :type full_sample_x, full_sample_y: lists
+    :return rx, ry: lists of x and y coordinates of the path, empty if no path
+    :rtype rx, ry: lists
+    """
+
+    start_node = Node(sx, sy, 0.0, -1)
+    goal_node = Node(gx, gy, 0.0, -1)
+
+    open_set, closed_set = dict(), dict()
+    open_set[len(full_road_map) - 2] = start_node
+
+    path_found = True
+
+    while True:
+        if not open_set:
+            print("Cannot find path")
+            path_found = False
+            break
+
+        c_id = min(open_set, key = lambda o: open_set[o].cost)
+        current = open_set[c_id]
+
+        if c_id == len(full_road_map) - 1:
+            print("Goal is found")
+            goal_node.parent_index = current.parent_index
+            goal_node.cost = current.cost
+            break
+
+        # remove visited node from the open set
+        del open_set[c_id]
+        # add it to the closed set containing visited nodes
+        closed_set[c_id] = current
+
+        # expand search grid
+        for i in xrange(len(full_road_map[c_id])):
+            n_id = full_road_map[c_id][i]
+            dx = full_sample_x[n_id] - current.x
+            dy = full_sample_y[n_id] - current.y
+            distance = math.hypot(dx, dy)
+
+            node = Node(x = full_sample_x[n_id], y = full_sample_y[n_id], \ 
+                        cost = current.cost + distance, c_id)
+            
+            if n_id in closed_set:
+                continue
+            # Otherwise the node has not been visited
+            if n_id in open_set:
+                if open_set[n_id].cost > node.cost:
+                    open_set[n_id].cost = node.cost
+                    open_set[n_id].parent_index = c_id
+            else:
+                open_set[n_id] = Node
+    # endWhile
+    if path_found is False:
+        return [], []
+
+    # generate the path
+    rx, ry = [goal_node.x], [goal_node.y]
+
+    parent_index = goal_node.parent_index
+    while (parent_index != -1):
+        n = closed_set[parent_index]
+        rx.append(n.x)
+        ry.append(n.y)
+        parent_index = n.parent_index
+    
+    return rx, ry
+
+
+
+
+
 
 # pl.show(block=True)   
 
