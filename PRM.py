328bce178f4b5dbb81a90547068b8c4ae7c10324
Toefinder <lequanganhok@gmail.com>
2020-10-13 17:56:19 +0800

    Added node class for graph

--- PRM.py
+++ PRM.py
@@ -90,7 +90,20 @@ def sample_points(size_x, size_y):
 
     return sample_x, sample_y
 
-        
+class Node:
+    """
+    Node class to form the graph for PRM
+    """      
+    def __init__(self, x, y, cost, parent_index):
+        self.x = x
+        self.y = y
+        self.cost = cost
+        self.parent_index = parent_index
+    
+    def __str__(self):
+        return str(self.x) + "," + str(self.y) + "," + \
+                str(self.cost) + "," + str(self.parent_index)
+
 
 # pl.show(block=True)   
 
