4e9641b03ecaa67f33eba2fc8432d20d36d65694
Toefinder <lequanganhok@gmail.com>
2020-10-13 17:49:35 +0800

    Added the file for testing individual functions

---
+++ testing.py
@@ -0,0 +1,36 @@
+import numpy as np 
+import pylab as pl
+import math
+from PRM import is_collision, sample_points
+import sys
+sys.path.append('osr_examples/scripts/')
+import environment_2d
+pl.ion()
+np.random.seed(4)
+env = environment_2d.Environment(10, 6, 5)
+pl.clf()
+env.plot()
+q = env.random_query()
+if q is not None:
+    x_start, y_start, x_goal, y_goal = q
+    env.plot_query(x_start, y_start, x_goal, y_goal)
+
+# # Testing is_collision
+# print("Testing is_collision")
+# sx = 1
+# sy = 0.5
+# gx = 4
+# gy = 1.5
+# rr = 0.2
+# print("sx = ", sx)
+# print("sy = ", sy)
+# print("gx = ", gx)
+# print("gy = ", gy)
+# print(is_collision(sx, sy, gx, gy, rr))
+
+# Testing sample_points
+sample_x, sample_y = sample_points(env.size_x, env.size_y)
+for i in xrange(len(sample_x)):
+    pl.plot([sample_x[i]], [sample_y[i]], "bo", markersize=5)
+
+pl.show(block = True)
